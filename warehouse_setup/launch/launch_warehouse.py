efe68cbac51fcd4f046f7358a79b6730af989f0e
Will Solow <soloww@oregonstate.edu>
2025-05-20 14:37:21 -0700

    kinova arm working!

--- warehouse_setup/launch/launch_warehouse.py
+++ warehouse_setup/launch/launch_warehouse.py
@@ -1,35 +1,100 @@
-from ament_index_python.packages import get_package_share_directory
+import os
+from ament_index_python.packages import get_package_prefix, get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import (DeclareLaunchArgument, SetEnvironmentVariable, 
-                            IncludeLaunchDescription, SetLaunchConfiguration)
-from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, TextSubstitution
-from launch_ros.actions import Node
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import (
+    AppendEnvironmentVariable,
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
 
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def launch_setup(context, *args, **kwargs):
+
+    sim_gazebo = LaunchConfiguration("sim_gazebo")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    spawn_arm = LaunchConfiguration("spawn_arm")
+
+    pkg_warehouseworld = get_package_share_directory('warehouse_world')
+    pkg_warehouse_setup = get_package_share_directory('warehouse_setup')
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
+    
+    gz_resource_path = AppendEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH", PathJoinSubstitution([pkg_warehouseworld, 'models'])
+    )
+    
+    gz_launch_description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(PathJoinSubstitution([pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])),
+        launch_arguments={
+            
+            "gz_args": f" -r -v 3 warehouse_world/worlds/warehouse2.sdf --physics-engine gz-physics-bullet-featherstone-plugin",
+            "on_exit_shutdown": "True"
+        }.items(),
+        condition=IfCondition(sim_gazebo),
+    )
+
+    # Kinova Arm Launch Description
+    kinova_arm_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(PathJoinSubstitution([pkg_warehouse_setup, 'launch', 'kortex_sim_control.launch.py'])),
+        launch_arguments={}.items(),
+        condition=IfCondition(spawn_arm),
+    )
+
+    nodes_to_launch = [
+        gz_resource_path,
+        gz_launch_description,
+        kinova_arm_launch
+    ]
+
+    return nodes_to_launch
 
 def generate_launch_description():
-    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-    pkg_warehouseworld_gz_sim = get_package_share_directory('warehouse_world')
-    gz_launch_path = PathJoinSubstitution([pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'])
-    gz_model_path = PathJoinSubstitution([pkg_warehouseworld_gz_sim, 'models'])
+   
 
-    return LaunchDescription([
+    declared_arguments = []
+
+    # Simulation specific arguments
+    declared_arguments.append(
         DeclareLaunchArgument(
-            'world',
-            default_value='warehouse',
-            choices=['warehouse'],
-            description='World to load into Gazebo'
-        ),
-        SetLaunchConfiguration(name='world_file', 
-                               value=[LaunchConfiguration('world'), 
-                                      TextSubstitution(text='.sdf')]),
-        SetEnvironmentVariable('GZ_SIM_RESOURCE_PATH', gz_model_path),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(gz_launch_path),
-            launch_arguments={
-                'gz_args': [PathJoinSubstitution([pkg_warehouseworld_gz_sim, 'worlds',
-                                                  LaunchConfiguration('world_file')])],
-                'on_exit_shutdown': 'True'
-            }.items(),
-        ),
-    ])
+            "sim_gazebo",
+            default_value="true",
+            description="Use Gazebo for simulation",
+        )
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value="true",
+            description="Use simulated clock",
+        )
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "worlds_file",
+            default_value="warehouse2.sdf",
+            description="World to launch",
+        )
+    )
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "spawn_arm",
+            default_value="true",
+            description="Spawn the Kinova Arm",
+        )
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
+
--- warehouse_setup/setup.py
+++ warehouse_setup/setup.py
@@ -3,16 +3,26 @@ import os
 from glob import glob
 package_name = 'warehouse_setup'
 
-setup(
-    name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
+data_files = [
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
-    ],
+    ]
+
+config_dir = ''
+
+for dirpath, dirnames, filenames in os.walk(config_dir):
+    for f in filenames:
+        full_path = os.path.join(dirpath, f)
+        install_path = os.path.join('share', package_name, dirpath)
+        data_files.append((install_path, [full_path]))
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files= data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Will Solow',
